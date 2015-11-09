dc2fe6da7df0159f80881a90c941d04866a190ca
Ben <bjlinville1@gmail.com>
2015-11-09 15:31:21 -0500

    attempting to fix a heroku precomilation error

--- app/assets/javascripts/designs.js
+++ app/assets/javascripts/designs.js
@@ -1,9 +1,9 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready(function() {
-  window.NestedFormEvents.prototype.insertFields = function(content, assoc, link) {
-    var $tr = $(link).closest('tr');
-    return $(content).insertBefore($tr);
-  }
-}
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+// $(document).ready(function() {
+//   window.NestedFormEvents.prototype.insertFields = function(content, assoc, link) {
+//     var $tr = $(link).closest('tr');
+//     return $(content).insertBefore($tr);
+//   }
+// }
